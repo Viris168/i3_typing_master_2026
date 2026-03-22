74b1a484544da2c1fd75f64aef1d4793e3166ab0
Viris168 <sokvirak0852@gmail.com>
2026-03-23 03:51:38 +0700

    new_fix

--- welcome_page.java
+++ welcome_page.java
@@ -1,11 +1,52 @@
-public class Welcome_menu_screen {
-    public static void main(String[] args){
-        System.out.println("___New user Registation___");
-        System.out.println("To begin, please select one of the following options:");
-        System.out.println("1. Login");
-        System.out.println("2. Register");
-        System.out.println("3. Exit");
-        System.out.println("Choose an option:");
+import java.util.Scanner;
 
+public class welcome_page {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean running = true;
+        while (running) {
+            System.out.println("\n------------- Welcome to I3 Typing Master v2026 -------------");
+            System.out.println("To Begin, please select one of the following options:");
+            System.out.println("1. User registering");
+            System.out.println("2. User reset password");
+            System.out.println("3. User login");
+            System.out.println("4. List of test results for a user");
+            System.out.println("5. User typing test");
+            System.out.println("6. About Us");
+            System.out.println("7. Exit");
+            System.out.print("Choose an option: - ");
+
+            String choice = sc.nextLine();
+
+            switch (choice) {
+                case "1":
+                    new Register().showRegistration();
+                    break;
+                case "2":
+                    new ResetPassword().show();
+                    break;
+                case "3":
+                    new Login().show();
+                    break;
+                case "4":
+                    new TestResults().displayResults();
+                    break;
+                case "5":
+                    new TypingTest().startTest();
+                    break;
+                case "6":
+                    new AboutUs().showInfo();
+                    break;
+                case "7":
+                    System.out.println("Goodbye!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid option. Please choose 1-7.");
+            }
+        }
+
+        sc.close();
     }
 }
