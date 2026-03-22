e4f4db444402c6bf8b7d23308b6673f20b22577c
Viris168 <sokvirak0852@gmail.com>
2026-03-23 03:03:15 +0700

    wecome page

---
+++ welcome_page.java
@@ -0,0 +1,11 @@
+public class Welcome_menu_screen {
+    public static void main(String[] args){
+        System.out.println("___New user Registation___");
+        System.out.println("To begin, please select one of the following options:");
+        System.out.println("1. Login");
+        System.out.println("2. Register");
+        System.out.println("3. Exit");
+        System.out.println("Choose an option:");
+
+    }
+}
